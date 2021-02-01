5d07aeee3c180dfab022a77fea61211a9b674db6
nlea <nele.uhlemann@posteo.de>
2021-02-01 10:06:31 +0100

    Change Telegram Message, just inclue new cases and week incidence as numbers

--- src/worker.ts
+++ src/worker.ts
@@ -17,20 +17,23 @@ const bot = new TelegramBot(apikey, {polling: true});
 zbc.createWorker("send-update", async (job, complete) => {  
 
     const { activity } = job.variables;
-    const { updateInformation: { cases } } = job.variables;
+    //const { updateInformation: { cases } } = job.variables;
+    const { updateInformation: { delta: {cases } } } = job.variables;
+
     const { updateInformation: { weekIncidence}} = job.variables;
-    const { updateInformation: {deaths}} = job.variables;
-    const { updateInformation: { recovered}} = job.variables;
+    //const { updateInformation: {deaths}} = job.variables;
+    //const { updateInformation: { recovered}} = job.variables;
 
     
     const smileyWithMask = " \u{1F637}";
     const pointingDown = "\u{1F447}";
     const stopSign ="\u{1F6D1}";
     const sunflower = "\u{1F33B}";
+    // + "<b>deaths: </b> \n" + deaths + "\n \n" + "<b>recorvered:</b> \n" + recovered +"\n \n"
     
 
     const text = "Hello there,  \n \n  Here comes the Corona update for Germany \n" + stopSign +" The situation is still serious: \n \n" +
-    "<b>cases:</b> \n" + cases +"\n \n"+ "<b>deaths: </b> \n" + deaths + "\n \n" + "<b>recorvered:</b> \n" + recovered +"\n \n" +
+    "<b>new registered cases within 24 hours:</b> \n" + cases +"\n \n" +
     "<b>week incidence:</b> \n" + weekIncidence + "\n \n" + "Take care and stay healthy " + smileyWithMask + "\n \n"+
     sunflower + "<b>Suggested lockdown activity for the day: </b>: \n" + activity + "\n\n" + pointingDown + "Provide feedback or post a picture of the activity" + pointingDown;
   
